d0c8ba67b8b332604484b4899d90244098285cd6
Tim Sullivan <timsullivan97@gmail.com>
2021-02-24 09:38:00 -0800

    add implicit type coercion exercises

---
+++ js/09_implicit-type-coercion-08.js
@@ -0,0 +1,11 @@
+'' + [1, 2, 3]
+// => '1,2,3'
+
+'' + true
+// => 'true'
+
+'' + undefined
+// => 'undefined'
+
+'' + {}
+// => '[object Object]'
---
+++ js/09_implicit-type-coercion-09.js
@@ -0,0 +1,6 @@
+1 + true; // 2
+1 + false; // 1
+true + false; // 1
+null + false; // 0
+null + null; // 0
+1 + undefined; // NaN
