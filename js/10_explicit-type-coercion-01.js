838096fd31a554c3baab3949457543bda64a6198
Tim Sullivan <timsullivan97@gmail.com>
2021-02-25 08:29:01 -0800

    type coercion and obj/primitive values exercises

---
+++ js/10_explicit-type-coercion-01.js
@@ -0,0 +1,12 @@
+let one = Number('1')
+one
+// => 1
+
+typeof one >
+// => "number"
+
+Number("cat");
+// => NaN
+
+Number('')
+// => 0
---
+++ js/11_object-and-primitive-values.js
@@ -0,0 +1,14 @@
+let number = 20
+let newNumber = number + 1
+newNumber
+// => 21
+
+number
+// => 20
+
+let object = { a: 1, b: 2, c: 3 }
+// => object.c = object.c + 1
+// => 4
+
+object
+// => { a: 1, b: 2, c: 4 }
